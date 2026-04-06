c7079150e959b0550c2f8929029250930a8bfb01
Amanda Cristina <amanda.rosa@sou.inteli.edu.br>
2026-04-06 19:17:44 -0300

    feat: create do CRUD

--- src/database.py
+++ src/database.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-DB_NAME = 'dados.db'
+DB = 'dados.db'
 # criação da conexão com o banco de dados e configuração para evitar bloqueios
 def get_db_connection():
-    conn = sqlite3.connect(DB_NAME, timeout=10)
+    conn = sqlite3.connect(DB, timeout=10)
     conn.execute('PRAGMA journal_mode=WAL')
     conn.execute('PRAGMA busy_timeout=5000')
     conn.row_factory = sqlite3.Row
@@ -16,3 +16,19 @@ def init_db():
     with open('schema.sql') as f:
         conn.executescript(f.read())
     conn.close()
+
+# CRUD
+# create
+def inserir_leitura(temperatura, umidade):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        'INSERT INTO leituras (temperatura, umidade) VALUES (?, ?)',
+        (temperatura, umidade)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return cursor.lastrowid
